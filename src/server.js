e685e2b35d00644fccaf81096983e95499bbc7a6
Cristovao-bit <63509410+Cristovao-bit@users.noreply.github.com>
2020-06-12 09:27:02 -0300

    origin

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const server = express();
 
+// Configurar caminhos da minha aplicação
+// Página inicial
+// Req: Requisição
+// Res: Resposta
+server.get("/", (req, res) => {
+    res.send("Cheguei aqui!");
+});
 
 // Ligar o servidor
 server.listen(3000);
