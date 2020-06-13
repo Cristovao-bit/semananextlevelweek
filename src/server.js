29e074aa7bd936dfbbc3db46b97cacd2eff6b91e
Cristovao-bit <63509410+Cristovao-bit@users.noreply.github.com>
2020-06-13 09:45:55 -0300

    origin

--- src/server.js
+++ src/server.js
@@ -4,13 +4,28 @@ const server = express();
 // Configurar pasta publica
 server.use(express.static("public"));
 
+// utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
 // Configurar caminhos da minha aplicação
 // Página inicial
 // Req: Requisição
 // Res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
 });
 
 // Ligar o servidor
-server.listen(3000);
+server.listen(3333);
